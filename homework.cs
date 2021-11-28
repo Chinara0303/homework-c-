84941dc246e25b2d00727e1c2feb0dc4ba3d6b57
[Chinara0303] <chinaraei@code.edu.az>
2021-11-28 13:19:36 +0400

    main

---
+++ homework.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ededin sade ve ya murekkeb olmasi
+            //int n = 8;
+            //if (n % 2 == 0) 
+
+            //{
+            //    Console.WriteLine("murekkeb");
+            //}
+            //else
+            //{
+            //   Console.WriteLine("sade");
+            //}
+
+            //int n = 23;
+            //for (int i = 1; i < 2; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        Console.WriteLine("MUREKKEB");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sade");
+            //    }
+
+
+
+            //ededin reqemlerinin cemi
+
+            //int n = 58;
+            //int sum = 0;
+            //int k;
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (n != 0) ;
+
+            //    k = n % 10;
+            //    sum = sum + k;
+            //    n = n / 10;
+
+            //}
+            //Console.WriteLine(sum);
+
+            //int n, sum = 0, m;
+            //Console.Write("Enter a number: ");
+            //n = int.Parse(Console.ReadLine());
+            //while (n > 0)
+            //{
+            //    m = n % 10;
+            //    sum = sum + m;
+            //    n = n / 10;
+            //}
+            //Console.Write("Sum is= " + sum);
+
+            //fesil
+
+          //int month = 9;
+            //switch (month)
+            //{
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Spring");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Summer");
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Autumn");
+            //        break;
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("Winter");
+            //        break;
+            //}
+
+    }  }                 
